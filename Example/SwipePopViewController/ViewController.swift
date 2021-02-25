be7aba29360c10b8727c60d84d8070565f0173c0
Athipat Nampetch <kanottonp@gmail.com>
2021-02-25 10:06:05 +0700

    Add some example between native gesture and SwipePopGesture

--- Example/SwipePopViewController/ViewController.swift
+++ Example/SwipePopViewController/ViewController.swift
@@ -10,21 +10,52 @@ import UIKit
 import SwipePopViewController
 
 class ViewController: UIViewController {
-
-    @IBAction func tapPushVC(_ sender: Any) {
+    
+    enum Mode: Int {
+        case native
+        case swipePop
+    }
+    
+    var mode: Mode = .native
+    
+    @IBAction func tapPushVC(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "viewController")
+        let vc = storyboard.instantiateViewController(withIdentifier: "viewController") as! ViewController
+        
+        vc.mode = Mode(rawValue: sender.tag) ?? .native
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        switch mode {
+        case .native:
+            applyNativePopGesture()
+        case .swipePop:
+            addSwipePopGesture()
+        }
+        self.title = "ViewController#\(self.navigationController!.viewControllers.count)"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Thread.sleep(forTimeInterval: 0.1)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        addSwipePopGesture()
+    }
+    
+    private func applyNativePopGesture() {
+        if (self.navigationController?.viewControllers.count ?? 0) > 1 {
+            let popGestureRecognizer = self.navigationController!.interactivePopGestureRecognizer!
+            if let targets = popGestureRecognizer.value(forKey: "targets") as? NSMutableArray {
+                let gestureRecognizer = UIPanGestureRecognizer()
+                gestureRecognizer.setValue(targets, forKey: "targets")
+                self.view.addGestureRecognizer(gestureRecognizer)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,4 +64,3 @@ class ViewController: UIViewController {
     }
 
 }
-
