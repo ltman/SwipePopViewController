9e299443681fb8e8f4b57ff9a3518414164c8384
chalermpong <s_pong_c@hotmail.com>
2021-02-27 22:36:42 +0700

    A solution using just PanGestureRecognizer

--- Example/SwipePopViewController/ViewController.swift
+++ Example/SwipePopViewController/ViewController.swift
@@ -40,21 +40,24 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        Thread.sleep(forTimeInterval: 0.1)
+        print("viewWillAppear")
+        Thread.sleep(forTimeInterval: 0.2)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    
     private func applyNativePopGesture() {
-        if (self.navigationController?.viewControllers.count ?? 0) > 1 {
-            let popGestureRecognizer = self.navigationController!.interactivePopGestureRecognizer!
-            if let targets = popGestureRecognizer.value(forKey: "targets") as? NSMutableArray {
-                let gestureRecognizer = UIPanGestureRecognizer()
-                gestureRecognizer.setValue(targets, forKey: "targets")
-                self.view.addGestureRecognizer(gestureRecognizer)
-            }
+        if let nc = self.navigationController {
+            let popGesture = PanInteractivePopGesture()
+            popGesture.addInteractivePopGesture(navigationController: nc, view: self.view)
         }
     }
 
@@ -63,4 +66,79 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+}
+
+class PanInteractivePopGesture: UIPanGestureRecognizer {
+
+    func addInteractivePopGesture(navigationController: UINavigationController,
+                                  view: UIView) {
+        if navigationController.viewControllers.count > 1 {
+            if let popGestureRecognizer = navigationController.interactivePopGestureRecognizer,
+               let targets = popGestureRecognizer.value(forKey: "targets") as? NSMutableArray {
+                self.setValue(targets, forKey: "targets")
+                self.addTarget(self, action: #selector(panTarget))
+                view.addGestureRecognizer(self)
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        print("touchesBegan")
+        super.touchesBegan(touches, with: event)
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+        print("touchesMoved at \(Date().timeIntervalSince(lastBeginTime))")
+        super.touchesMoved(touches, with: event)
+        if state == .began {
+            firstTranslation = translation(in: nil)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+        print("touchesEnded")
+        super.touchesEnded(touches, with: event)
+        firstTranslation = .zero
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+        print("touchesCancelled")
+        super.touchesCancelled(touches, with: event)
+        firstTranslation = .zero
+    }
+    
+    var firstTranslation: CGPoint = .zero
+    var lastBeginTime = Date()
+    @objc func panTarget(_ gestureRecognizer: UIPanGestureRecognizer) {
+        print("GestureState=\(gestureRecognizer.state.rawValue)")
+        switch gestureRecognizer.state {
+        case .began:
+            lastBeginTime = Date()
+        default:
+            break
+        }
+        //self.state = gestureRecognizer.state
+    }
+    
+    override func translation(in view: UIView?) -> CGPoint {
+        if state == .began {
+            print("")
+        }
+        let t = super.translation(in: view)
+        print("translation \(t) at \(Date().timeIntervalSince(lastBeginTime))")
+        return CGPoint(x: t.x - firstTranslation.x, y: t.y - firstTranslation.y)
+    }
+
+    override func velocity(in view: UIView?) -> CGPoint {
+        let v = super.velocity(in: view)
+        print("velocity \(v) at \(Date().timeIntervalSince(lastBeginTime))")
+        return v
+    }
+
+    
+    deinit {
+        print("deinit")
+    }
 }
